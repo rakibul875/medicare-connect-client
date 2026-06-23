e2835f11d570e9469ce0362c853daeabcf7ee2e6
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-23 21:59:58 +0600

    refactor DoctorDashboard component; improve code formatting and structure for better readability

--- src/app/dashboard/doctor/page.jsx
+++ src/app/dashboard/doctor/page.jsx
@@ -1,3 +1,4 @@
+import DoctorAppointmentList from "@/components/dashboard/doctor/DoctorAppointmentList";
 import HomeCard from "@/components/dashboard/doctor/HomeCard";
 import { getTodayAppointment } from "@/lib/api/getAppointment";
 import { getUserSession } from "@/lib/api/getUsers";
@@ -10,18 +11,28 @@ const DoctorDashboard = async () => {
 
   const paymentData = await getPaymentHistoryDoctor(doctorId);
   let totalAmount = 0;
-  for (let i = 0; i < paymentData.length; i++){
-    totalAmount +=paymentData[i].amount
+  for (let i = 0; i < paymentData.length; i++) {
+    totalAmount += paymentData[i].amount;
   }
-  const totalPatient= paymentData.length
-  const todayAppointment= await getTodayAppointment(doctorId)
-  const todayAppointmentLength= todayAppointment.length
-  
-    return (
-      <div>
-        <HomeCard totalAmount={totalAmount} totalPatient={totalPatient} todayAppointment={todayAppointmentLength}/>
+  const totalPatient = paymentData.length;
+  const todayAppointment = await getTodayAppointment(doctorId);
+  const todayAppointmentLength = todayAppointment.length;
+
+  return (
+    <div>
+      <HomeCard
+        totalAmount={totalAmount}
+        totalPatient={totalPatient}
+        todayAppointment={todayAppointmentLength}
+      />
+      <div className="">
+        <div className="">
+          <h1 className="text-3xl font-bold">Today Appointments</h1>
+          <DoctorAppointmentList appointments={todayAppointment} />
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default DoctorDashboard;
